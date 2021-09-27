4fdc9b450bbccd1d365c0867ad2a5922e3804179
Fvino <86255196+Fvino@users.noreply.github.com>
2021-09-28 00:54:31 +0200

    init

---
+++ MyProgectGB/MyProgectGB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyProgectGB
+//
+//  Created by Valeriy Fomin on 28/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
